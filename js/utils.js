af4758500a260ebe0abfe1b48e57e0f2423762f7
Inna Aleksenitser <148863587+Inna-B10@users.noreply.github.com>
2024-12-09 13:00:49 +0100

    truncate text, but include the whole last word

--- js/utils.js
+++ js/utils.js
@@ -2,5 +2,13 @@ export function truncateText(text, maxLength = 50) {
 	if (text.length <= maxLength) {
 		return text
 	}
-	return text.slice(0, maxLength) + '[..]'
+
+	const truncated = text.slice(0, maxLength + 1)
+	const lastSpaceIndex = truncated.lastIndexOf(' ')
+
+	if (lastSpaceIndex === -1) {
+		return text.slice(0, maxLength) + ' [..]'
+	}
+
+	return text.slice(0, lastSpaceIndex) + ' [..]'
 }
