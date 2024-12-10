647465e2f902a519e21f3845efc0748f9162cbae
banjohans <hansmartinaustestad@gmail.com>
2024-12-10 08:37:22 +0100

    third commit, test PUT approved

--- MyApi/obj/Debug/net8.0/MyApi.AssemblyInfo.cs
+++ MyApi/obj/Debug/net8.0/MyApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0702affa35f053dfdc40402484adbf78e35cb6d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68cd55fc027db0fe7ee2ba49340b96a88b0026d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
